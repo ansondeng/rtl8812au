8bc17239d9ea7a30b96e2556c0decfa69632fc0a
ansondeng <ansondeng3000@gmail.com>
2022-09-14 13:30:30 +0800

    github.com/greearb/rtl8812AU_8821AU_linux  661268fd174d4a74834c82d7d3987b0a560e6c57

--- include/rtw_event.h
+++ include/rtw_event.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- * Copyright(c) 2007 - 2017 Realtek Corporation.
+ * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
@@ -11,19 +11,24 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
- *****************************************************************************/
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ *
+ *
+ ******************************************************************************/
 #ifndef _RTW_EVENT_H_
 #define _RTW_EVENT_H_
 
 #ifdef CONFIG_H2CLBK
-	#include <h2clbk.h>
+#include <h2clbk.h>
 #endif
 
 /*
 Used to report a bss has been scanned
 
 */
-struct survey_event	{
+struct survey_event {
 	WLAN_BSSID_EX bss;
 };
 
@@ -36,7 +41,7 @@ bss_cnt indicates the number of bss that has been reported.
 */
 struct surveydone_event {
 	unsigned int	bss_cnt;
-	bool acs; /* aim to trigger channel selection */
+
 };
 
 /*
@@ -61,13 +66,19 @@ It is used in AP/Ad-HoC(M) mode.
 */
 struct stassoc_event {
 	unsigned char macaddr[6];
+	unsigned char rsvd[2];
+	int    cam_id;
+
 };
 
 struct stadel_event {
 	unsigned char macaddr[6];
-	unsigned char rsvd[2]; /* for reason */
-	unsigned char locally_generated;
+	unsigned char rsvd[2]; //for reason
 	int mac_id;
+};
+
+struct addba_event {
+	unsigned int tid;
 };
 
 struct wmm_event {
@@ -85,10 +96,43 @@ struct c2hlbk_event {
 	unsigned char	b1;
 	unsigned int	w1;
 };
-#endif/* CONFIG_H2CLBK */
+#endif//CONFIG_H2CLBK
 
-struct rtw_event {
-	u32 parmsize;
+#define GEN_EVT_CODE(event)	event ## _EVT_
+
+
+
+struct fwevent {
+	u32	parmsize;
 	void (*event_callback)(_adapter *dev, u8 *pbuf);
 };
-#endif /* _WLANEVENT_H_ */
+
+
+#define C2HEVENT_SZ			32
+
+struct event_node {
+	unsigned char *node;
+	unsigned char evt_code;
+	unsigned short evt_sz;
+	volatile int	*caller_ff_tail;
+	int	caller_ff_sz;
+};
+
+struct c2hevent_queue {
+	volatile int	head;
+	volatile int	tail;
+	struct	event_node	nodes[C2HEVENT_SZ];
+	unsigned char	seq;
+};
+
+#define NETWORK_QUEUE_SZ	4
+
+struct network_queue {
+	volatile int	head;
+	volatile int	tail;
+	WLAN_BSSID_EX networks[NETWORK_QUEUE_SZ];
+};
+
+
+#endif // _WLANEVENT_H_
+
